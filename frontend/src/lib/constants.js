8462e219ef8ab4a15c95bcb1a9443b4c107c2363
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-07-10 08:51:54 +0900

    Update WEB_NAME constant

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
 
-export const WEB_NAME = 'Knowslog Template Web';
+export const WEB_NAME = '국민건강보험 일산병원 챗봇';
 export const FASTAPI_BASE_URL = dev ? 'http://localhost:8000' : '';
 export const API_URL = `${FASTAPI_BASE_URL}/api/v1`;
