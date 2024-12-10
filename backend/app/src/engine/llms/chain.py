5130314000cf130c6174d3abb602ff95cab28cac
Tourbut <42790398+tourbut@users.noreply.github.com>
2024-12-10 17:17:04 +0900

    Update search parameters and add map rerank chain

--- backend/app/src/engine/llms/chain.py
+++ backend/app/src/engine/llms/chain.py
@@ -72,8 +72,6 @@ def summarize_chain(api_key:str,
     chain = prompt|llm
     return chain
 
-
-
 def chatbot_chain(api_key:str,
                   model:str='gpt-4o-mini',
                   temperature:float=0.7,
@@ -121,7 +119,48 @@ def chatbot_chain(api_key:str,
         prompt = get_chatbot_prompt()
         chain = prompt|llm
         return chain
+
+def map_rerank_chain(llm):
+    from langchain_core.prompts import PromptTemplate
+    from langchain_core.output_parsers import PydanticOutputParser
+    from pydantic import BaseModel, Field
+    class AnswerWithScore(BaseModel):
+        '''
+        The user's question and the score and reasons for scoring the answer.
+        '''
+        score: float = Field( ..., title="Score from 1.0 - 10.0. 10.0 is the best score.")
+        Reasons_for_scoring: str = Field( ..., title="Reasons for scoring.")
+        
     
+    parser = PydanticOutputParser(pydantic_object=AnswerWithScore)
+    
+    prompt_template = """
+    <INSTRUCTION>
+    Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+    If the answer answers the user question the score should be high, else it should be low.
+    On a scale of 1.0 to 10.0, write whether you have enough context to answer the user's question.
+    </INSTRUCTION>
+    <QUESTION>
+    {input} 
+    </QUESTION>
+    <CONTEXT> 
+    {context}
+    </CONTEXT>
+    <FORMAT>
+    {format_instructions}
+    </FORMAT>
+    """
+    
+    prompt = PromptTemplate(
+        template=prompt_template,
+        input_variables=["input", "context"],
+        partial_variables={"format_instructions": parser.get_format_instructions()}
+    )
+    
+    map_chain = prompt | llm | parser
+    return map_chain
+                        
+
 def thinking_chatbot_chain(api_key:str,
                            source:str,
                            model:str='gpt-4o-mini',
@@ -148,7 +187,7 @@ def thinking_chatbot_chain(api_key:str,
                             callback_manager=callback_manager)
     elif source == 'ollama':
         llm = ChatOllama(model=model,
-                         base_url= "http://192.168.1.73:11434",
+                         base_url= settings.OLLAMA_URL,
                          temperature=temperature,
                          callback_manager=callback_manager,
                          #format="json",
@@ -169,19 +208,35 @@ def thinking_chatbot_chain(api_key:str,
     
     think_chain = runnable|think_prompt|llm|think_parser
     answer_chain = prompt|llm
-
+    rerank_chain = map_rerank_chain(llm)
+    
     def get_thought(output):
         return output["thought"].THOUGHT
     
     def get_context(output):
         if retriever:
             docs = retriever.invoke(output["thought"].search_msg)
-            rtn = f"""검색어 : {output["thought"].search_msg} 
-            검색결과
-            """
-            for i in range(len(docs)):
-                rtn = rtn + "\n\n-------- page : "+str(i)+" --------\n\n" + docs[i].page_content
+            inputs = []
+            for doc in docs:
+                inputs.append({"input":output["thought"].search_msg,"context": doc.page_content})
+            
+            results = rerank_chain.batch(inputs,
+                                         config={"max_concurrency": 3},)
+            idx_max = results.index(max(results,key=lambda x:x.score))
+            
+            result = results[idx_max]
+            
+            if result.score <= 6:
+                return "해당 문서 없음"
+            else :
                 
+                rtn = f"""검색어 : {output["thought"].search_msg}
+                검색 문서 Score: {results[idx_max].score}점(10점 만점)
+                Score 측정 사유:{results[idx_max].Reasons_for_scoring}
+                검색결과
+                """
+                rtn = rtn + "\n\n-------- page : "+str(idx_max)+" --------\n\n" + docs[idx_max].page_content
+            
             return rtn
         else:
             return ""
