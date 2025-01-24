c9768b7a701ea8ab164176a85f3489fe1dc13d0a
Tourbut <42790398+tourbut@users.noreply.github.com>
2025-01-24 14:50:23 +0900

    Refactor Sidebar component to improve readability and add scroll functionality

--- frontend/src/lib/components/common/Sidebar.svelte
+++ frontend/src/lib/components/common/Sidebar.svelte
@@ -20,8 +20,8 @@
     
 </script>
 <div class="relative">
-    <Sidebar >
-        <SidebarWrapper>
+    <Sidebar class="max-h-screen"> <!-- 화면 전체 높이를 최대값으로 설정 -->
+        <SidebarWrapper class="h-full overflow-y-auto max-h-[calc(88vh-2rem)]"> <!-- 스크롤 가능한 영역 설정 -->
             <SidebarGroup>
                 {#if (btn_add_button != undefined)}
                 <SidebarItem label={create_btn_name} on:click={btn_add_button}>
@@ -31,28 +31,52 @@
                 </SidebarItem>
                 {/if}
                 {#each side_menus as menu (menu.category)}
-                
                     {#each menu.items as item}
-                    <div style="display: flex; align-items: center;">
-                        <SidebarItem id={item.id} label={item.label.substring(0,18)+"..."} href={item.herf} 
-                                     on:click={() => {btn_click(item.id); is_hidden = true;}} />
-                        <DotsHorizontalOutline class="dots-menu dark:text-white" />
-                        <Dropdown triggeredBy=".dots-menu">
-                        <DropdownItem>공유</DropdownItem>
-                        <DropdownItem slot="footer" class="text-red-600" on:click={btn_item_more_click(item.id)}>
-                            삭제
-                        </DropdownItem>
-                        </Dropdown>
+                    <div class="flex items-center justify-between w-full px-3">
+                        <SidebarItem 
+                                spanClass="w-[180px] h-[20px] overflow-hidden" 
+                                id={item.id} 
+                                label={item.label} 
+                                href={item.herf} 
+                                on:click={() => {btn_click(item.id); is_hidden = true;}}
+                            />
+                            <Tooltip target={item.id} placement="bottom">
+                                <P color="text-white-700" size="sm" weight="black">{item.label}</P>
+                                {#if item.caption}
+                                    <P color="text-white-700" size="xs">({item.caption})</P>
+                                {/if}
+                            </Tooltip>
+                        <DotsHorizontalOutline class="dots-menu dark:text-white w-6 h-6 cursor-pointer" />
                     </div>
-                    <Tooltip target={item.id} placement="bottom">
-                        <P color="text-white-700" size="sm" weight="black">{item.label}</P>
-                        {#if item.caption}
-                        <P color="text-white-700" size="xs">({item.caption})</P>
-                        {/if}
-                    </Tooltip>
+                    <Dropdown triggeredBy=".dots-menu" class="w-[100px]">
+                    <DropdownItem>공유</DropdownItem>
+                    <DropdownItem slot="footer" class="text-red-600" on:click={btn_item_more_click(item.id)}>
+                        삭제
+                    </DropdownItem>
+                    </Dropdown>
                     {/each}
                 {/each}
             </SidebarGroup>
         </SidebarWrapper>
     </Sidebar>
 </div>
+
+<style>
+    /* 스크롤바 스타일링 (선택사항) */
+    :global(.overflow-y-auto::-webkit-scrollbar) {
+        width: 4px;
+    }
+    
+    :global(.overflow-y-auto::-webkit-scrollbar-track) {
+        background: transparent;
+    }
+    
+    :global(.overflow-y-auto::-webkit-scrollbar-thumb) {
+        background: #888;
+        border-radius: 2px;
+    }
+    
+    :global(.overflow-y-auto::-webkit-scrollbar-thumb:hover) {
+        background: #555;
+    }
+</style>
