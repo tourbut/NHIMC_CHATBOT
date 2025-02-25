65f23bb10cfd2af3db0463f424f44260075abd36
Tourbut <42790398+tourbut@users.noreply.github.com>
2025-02-25 10:09:51 +0900

    chat.py에서 불필요한 코드 제거 및 의존성 정리

--- backend/app/src/routes/chat.py
+++ backend/app/src/routes/chat.py
@@ -4,29 +4,22 @@ from typing import Any,List
 
 from fastapi import APIRouter, HTTPException, Request
 
-from app.src.deps import SessionDep_async,CurrentUser,async_engine,engine, redis_client
+from app.src.deps import SessionDep_async,CurrentUser,engine, redis_client
 from app.src.crud import chat as chat_crud
 from app.src.schemas import chat as chat_schema
 from app.src.crud import pgvector as pgvector_crud
-from app.src.schemas import pgvector as pgvector_schema
 from fastapi.responses import StreamingResponse
 from app.core.config import settings
 from app.src.engine.llms.chain import (
-    translate_chain,
-    summarize_chain,
     thought_chatbot_chain,
     chatbot_chain,
-    thinking_chatbot_chain,
-    thinking_chatbot_NoDoc_chain
 )
-from app.src.engine.llms.memory import pg_vetorstore_with_memory, pg_vetorstore,pg_ParentDocumentRetriever,clear_memory
+from app.src.engine.llms.memory import pg_vetorstore_with_memory,pg_ParentDocumentRetriever,clear_memory
 from datetime import datetime
-from requests.exceptions import RequestException
 from langchain_redis import RedisChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import (
     AIMessage,
-    BaseMessage,
     HumanMessage,
 )
 from langchain_community.callbacks import get_openai_callback
@@ -48,176 +41,6 @@ async def create_chat(*, session: SessionDep_async, current_user: CurrentUser,ch
 async def get_chat_list(*, session: SessionDep_async, current_user: CurrentUser):
     chats = await chat_crud.get_chat_list(session=session,current_user=current_user)
     return chats
-
-@router.post("/send_message",response_model=chat_schema.OutMessage)
-async def send_message(*,session: SessionDep_async, current_user: CurrentUser,chat_in: chat_schema.SendMessage):
-    
-    # Get userllm
-    llm_with_embbeding = await chat_crud.get_llm(session=session,user_id=current_user.id)
-    llm = None
-    embedding = None
-    
-    for item in llm_with_embbeding:
-        if item.llm_id in [chat_in.user_llm_id,chat_in.dept_llm_id] and item.type == "llm":
-            llm = item
-        elif chat_in.user_llm_id is not None and item.gubun == 'user' and item.type == "embedding":
-            embedding = item
-        elif chat_in.dept_llm_id is not None and item.gubun == 'dept' and item.type == "embedding":
-            embedding = item
-            
-    # Get a postgres vectorstore with memory
-    memory = pg_vetorstore_with_memory(connection=engine,
-                                       collection_name=chat_in.chat_id.hex,
-                                       api_key=settings.GLOBAL_EMBEDDING_API,
-                                       source=settings.GLOBAL_EMBEDDING_SOURCE,
-                                       model=settings.GLOBAL_EMBEDDING_MODEL,
-                                       base_url=settings.GLOBAL_EMBEDDING_URL,
-                                       search_kwargs={"k": 3})
-    
-    retriever = None
-    document = None
-    if chat_in.document_id is not None:
-        document = await chat_crud.get_document(session=session,user_file_id=chat_in.document_id)
-        collection = await pgvector_crud.get_collection(session=session,collection_id=document.collection_id)
-        retriever = pg_ParentDocumentRetriever(connection=engine,
-                                    collection_name=collection.name,
-                                    api_key=embedding.api_key,
-                                    source=embedding.source,
-                                    model=embedding.name,
-                                    base_url=embedding.url,
-                                    async_mode=False,
-                                    splitter_options=collection.cmetadata
-                                    )
-    redis = await redis_client()
-    # Get or create a RedisChatMessageHistory instance
-    history = get_redis_history(chat_in.chat_id.hex,redis)
-    
-    user_message = chat_schema.CreateMessage(user_id=current_user.id,
-                                             chat_id=chat_in.chat_id,
-                                             name=current_user.name,
-                                             content=chat_in.input,
-                                             is_user=True,
-                                             create_date=datetime.now(),
-                                             update_date=datetime.now())
-    messages = []
-    messages.append(user_message)
-    
-    document_meta = "관련 문서 없음" if document is None else {'title':document.title,
-                                                              'description':document.description,}
-    if document is None:
-        #제공된 문서가 없는 경우
-        chain = thinking_chatbot_NoDoc_chain(api_key=llm.api_key,
-                                             source=llm.source,
-                                             model=llm.name,
-                                             base_url=llm.url,
-                                             memory=memory,
-                                             )
-    else:
-        #제공된 문서가 있는 경우
-        chain = thinking_chatbot_chain(api_key=llm.api_key,
-                                        source=llm.source,
-                                        model=llm.name,
-                                        base_url=llm.url,
-                                        memory=memory,
-                                        document_meta=document_meta,
-                                        retriever=retriever,)
-    
-    async def chain_astream(chain,input):
-    
-        chunks=[]
-        thought = None
-        input_token=0
-        output_token=0
-        
-        if llm.source == "openai":
-            callback_handler = get_openai_callback()
-            with callback_handler as cb:
-                async for chunk in chain.astream({'input':input}):
-                    thought = chunk['thought']
-                    answer = chunk['answer']
-                    chunks.append(answer)
-                    yield chat_schema.OutMessage(content=answer.content,
-                                                thought=None,
-                                                tools = {'retriever': ''},
-                                                input_token=None,
-                                                output_token=None,
-                                                is_done=False).model_dump_json()
-                input_token = cb.prompt_tokens
-                output_token = cb.completion_tokens
-        else:
-            async for chunk in chain.astream({'input':input}):
-                thought = chunk['thought']
-                answer = chunk['answer']
-                chunks.append(answer)
-                yield chat_schema.OutMessage(content=answer.content,
-                                            thought=None,
-                                            tools = {'retriever': ''},
-                                            input_token=None,
-                                            output_token=None,
-                                            is_done=False).model_dump_json()
-            
-            input_token = chunks[0].usage_metadata['input_tokens']
-            output_token = chunks[0].usage_metadata['output_tokens']
-            
-        response=chunks[0]
-        
-        for chunk in chunks[1:]:
-            response+=chunk
-        
-        usage = chat_schema.Usage(user_llm_id=chat_in.user_llm_id,
-                                  dept_llm_id=chat_in.dept_llm_id,
-                                  input_token=input_token,
-                                  output_token=output_token)
-        
-        bot_message = chat_schema.CreateMessage(user_id=current_user.id,
-                        chat_id=chat_in.chat_id,
-                        name="바르미",
-                        content=response.content,
-                        thought=thought.get('thought', ''),
-                        tools=json.dumps({'retriever': thought.get('context', '') }, ensure_ascii=False),
-                        is_user=False,
-                        create_date=datetime.now(),
-                        update_date=datetime.now())
-        
-        messages.append(bot_message)
-        
-        await chat_crud.create_messages(session=session,messages=messages,usage=usage)
-
-        # Add messages to chat history
-        await history.aadd_messages([HumanMessage(content=user_message.content,
-                                                  additional_kwargs={
-                                                      "user_id":str(user_message.user_id),
-                                                      "name":user_message.name,
-                                                      "create_date":user_message.create_date.strftime("%Y-%m-%d %H:%M:%S")
-                                                      }),])
-        
-        await history.aadd_messages([AIMessage(content=str(bot_message.content),
-                                               additional_kwargs={
-                                                   "user_id":str(bot_message.user_id),
-                                                   "name":bot_message.name,
-                                                   "thought":bot_message.thought,
-                                                   "tools" : json.loads(bot_message.tools),
-                                                   "create_date":bot_message.create_date.strftime("%Y-%m-%d %H:%M:%S")
-                                                   })])
-        
-        memory.save_context(
-            inputs={
-                "human": user_message.content
-                },
-            outputs={
-                "ai": bot_message.content
-                },
-            )
-        
-        yield chat_schema.OutMessage(content=bot_message.content,
-                                     thought=bot_message.thought,
-                                     tools = json.loads(bot_message.tools),
-                                     input_token=input_token,
-                                     output_token=output_token,
-                                     create_date=bot_message.create_date.strftime("%Y-%m-%d %H:%M:%S"),
-                                     is_done=True).model_dump_json()
-        
-    return StreamingResponse(chain_astream(chain,chat_in.input),media_type='text/event-stream')
 
 @router.get("/get_messages",response_model=List[chat_schema.ReponseMessages])
 async def get_messages(*, session: SessionDep_async, current_user: CurrentUser, id:uuid.UUID):
--- frontend/src/lib/apis/chat.js
+++ frontend/src/lib/apis/chat.js
@@ -1,10 +1,5 @@
 import fastapi from "$lib/fastapi";
 
-export async function send_message(params, success_callback, failure_callback,streamCallback) {
-    let url = "/chat/send_message/"
-    await fastapi('stream', url, params,success_callback,failure_callback,streamCallback)
-}
-
 export async function create_chat(params, success_callback, failure_callback) {
     let url = "/chat/create_chat/"
     await fastapi('post', url, params,success_callback,failure_callback)
